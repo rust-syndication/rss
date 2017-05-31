2bc1dc325464543f4ff5ecb65c08ab111440b7d3
Corey Farwell <coreyf@rwell.org>
2017-05-30 20:54:03 -0400

    Fix typo.

--- src/lib.rs
+++ src/lib.rs
@@ -96,7 +96,7 @@
 //!
 //! The the following checks are performed during validation:
 //!
-//! * Ensures that integer properties can be parsed from their string representaiton in to
+//! * Ensures that integer properties can be parsed from their string representaiton into
 //! integers
 //! * Ensures that the integer properties are within their valid range according to the RSS 2.0
 //! specification
