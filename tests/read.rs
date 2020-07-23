c9452826a6887c5876ee06349c7a26e4e7163d7d
David McHealy <david@simplecast.com>
2020-07-23 17:16:04 -0400

    Please rust fmt, hopefully.

--- tests/read.rs
+++ tests/read.rs
@@ -625,10 +625,7 @@ fn read_itunes() {
         channel.itunes_ext().unwrap().keywords(),
         Some("key1,key2,key3")
     );
-    assert_eq!(
-        channel.itunes_ext().unwrap().r#type(),
-        Some("episodic")
-    );
+    assert_eq!(channel.itunes_ext().unwrap().r#type(),Some("episodic"));
 
     assert_eq!(
         channel
