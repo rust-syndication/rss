eda164fd006fb61a18d2cc829a0dfcdebc226254
Andrey Kutejko <andy128k@gmail.com>
2021-01-07 15:25:43 +0100

    Bump copyright year

--- src/channel.rs
+++ src/channel.rs
@@ -1,6 +1,6 @@
 // This file is part of rss.
 //
-// Copyright © 2015-2017 The rust-syndication Developers
+// Copyright © 2015-2021 The rust-syndication Developers
 //
 // This program is free software; you can redistribute it and/or modify
 // it under the terms of the MIT License and/or Apache 2.0 License.
