6b3dead2bbd5320309203f588df3a871d0fac2d0
andy128k <andy128k@gmail.com>
2021-10-21 22:17:05 +0000

    Deploying to gh-pages from  @ 24eb17453b8b2b307842c7aac06df8da0bd423e3 🚀

--- implementors/std/io/trait.Read.js
+++ implementors/std/io/trait.Read.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["never"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"enum\" href=\"never/enum.Never.html\" title=\"enum never::Never\">Never</a>","synthetic":false,"types":["never::Never"]}];
+implementors["never"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"enum\" href=\"never/enum.Never.html\" title=\"enum never::Never\">Never</a>","synthetic":false,"types":["never::Never"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
