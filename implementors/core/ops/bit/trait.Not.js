966f2ea60bfcde8cb3630ccd320784440ee6e943
andy128k <andy128k@gmail.com>
2022-04-15 07:43:38 +0000

    Deploying to gh-pages from  @ 0824446f4a6df829ba2e4edfc80bc77015a11adc 🚀

--- implementors/core/ops/bit/trait.Not.js
+++ implementors/core/ops/bit/trait.Not.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["darling_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.56.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"darling_core/util/struct.Flag.html\" title=\"struct darling_core::util::Flag\">Flag</a>","synthetic":false,"types":["darling_core::util::Flag"]}];
+implementors["darling_core"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.60.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"struct\" href=\"darling_core/util/struct.Flag.html\" title=\"struct darling_core::util::Flag\">Flag</a>","synthetic":false,"types":["darling_core::util::Flag"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
