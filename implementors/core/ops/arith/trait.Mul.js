2ea56b037ff98d24ccf4bae4bfb62b1efd367e81
andy128k <andy128k@gmail.com>
2023-03-27 16:48:09 +0000

    Deploying to gh-pages from  @ 72909a3bac646c8ecbc38b0d623e5aa114d983a1 🚀

--- implementors/core/ops/arith/trait.Mul.js
+++ implementors/core/ops/arith/trait.Mul.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {
-"chrono":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.66.1/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.66.1/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"chrono/struct.Duration.html\" title=\"struct chrono::Duration\">Duration</a>"]]
+"chrono":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.68.1/core/ops/arith/trait.Mul.html\" title=\"trait core::ops::arith::Mul\">Mul</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/1.68.1/std/primitive.i32.html\">i32</a>&gt; for <a class=\"struct\" href=\"chrono/struct.Duration.html\" title=\"struct chrono::Duration\">Duration</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- quick_xml/writer/sidebar-items.js
+++ quick_xml/writer/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["ElementWriter","A struct to write an element. Contains methods to add attributes and inner elements to the element"],["Writer","XML writer. Writes XML [`Event`]s to a [`std::io::Write`] implementor."]]};
+window.SIDEBAR_ITEMS = {"struct":[["ElementWriter","A struct to write an element. Contains methods to add attributes and inner elements to the element"],["Writer","XML writer. Writes XML [`Event`]s to a [`std::io::Write`] or [`tokio::io::AsyncWrite`] implementor."]]};
