4c9343b81831cd44efd2a18d60163c48fcb89e21
andy128k <andy128k@gmail.com>
2023-07-26 01:18:16 +0000

    Deploying to gh-pages from  @ d6cbf8a1c721b0ed43b7104cbfd513c8036c16a6 🚀

--- implementors/alloc/string/trait.ToString.js
+++ implementors/alloc/string/trait.ToString.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"atom_syndication":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"atom_syndication/struct.Feed.html\" title=\"struct atom_syndication::Feed\">Feed</a>"]],
-"rss":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.69.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"rss/struct.Channel.html\" title=\"struct rss::Channel\">Channel</a>"]]
+"atom_syndication":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"atom_syndication/struct.Feed.html\" title=\"struct atom_syndication::Feed\">Feed</a>"]],
+"rss":[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.71.0/alloc/string/trait.ToString.html\" title=\"trait alloc::string::ToString\">ToString</a> for <a class=\"struct\" href=\"rss/struct.Channel.html\" title=\"struct rss::Channel\">Channel</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- quick_xml/sidebar-items.js
+++ quick_xml/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["Error"],"mod":["encoding","escape","events","name","reader","writer"],"struct":["NsReader"],"type":["Result"]};
+window.SIDEBAR_ITEMS = {"enum":["Error"],"mod":["encoding","escape","events","name","reader","writer"],"type":["Result"]};
