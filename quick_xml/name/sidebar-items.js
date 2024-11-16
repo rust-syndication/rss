452909f1048838d484c0e879df3ea5d7ebac0fc3
andy128k <andy128k@gmail.com>
2024-11-16 01:03:58 +0000

    Deploying to gh-pages from  @ 290bc1e13d0b6142d6b5f8c55693ae2de2e1966a 🚀

--- quick_xml/name/sidebar-items.js
+++ quick_xml/name/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["PrefixDeclaration","ResolveResult"],"struct":["LocalName","Namespace","Prefix","PrefixIter","QName"]};
+window.SIDEBAR_ITEMS = {"enum":["NamespaceError","PrefixDeclaration","ResolveResult"],"struct":["LocalName","Namespace","Prefix","PrefixIter","QName"]};
