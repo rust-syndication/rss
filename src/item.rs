5218e2994f5754022051dcb34da3581b77ffffc0
Andrey Kutejko <andy128k@gmail.com>
2026-05-10 14:26:12 +0200

    Replace Never with Infallible

--- src/item.rs
+++ src/item.rs
@@ -39,7 +39,7 @@ use crate::util::{decode, element_text, skip};
     builder(
         setter(into),
         default,
-        build_fn(name = "build_impl", private, error = "never::Never")
+        build_fn(name = "build_impl", private, error = "std::convert::Infallible")
     )
 )]
 pub struct Item {
